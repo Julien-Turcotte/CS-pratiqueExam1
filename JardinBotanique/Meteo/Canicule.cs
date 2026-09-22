f9e65699689b526d1dc01d1aca3db40f6e92ac24
Julien Turcotte <6315237@cegepoutaouais.qc.ca>
2026-09-22 15:12:51 -0400

    Initial commit

---
+++ JardinBotanique/Meteo/Canicule.cs
@@ -0,0 +1,19 @@
+﻿using JardinBotanique.ContenuJardin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JardinBotanique.Meteo
+{
+    // TODO: Corriger l'erreur puis compléter le code pour répoudre à l'exigence suivante :
+    //       En cas d'épisode de canicule, chaque plante consomme le double de son besoin quotidien en eau.
+    public class Canicule : Intemperie
+    {
+        public Canicule() : base("Canicule")
+        {
+
+        }
+    }
+}
---
+++ JardinBotanique/Meteo/Intemperie.cs
@@ -0,0 +1,15 @@
+﻿using JardinBotanique.ContenuJardin;
+
+namespace JardinBotanique.Meteo
+{
+    public abstract class Intemperie
+    {
+        public string Nom { get; }
+        public Intemperie(string nom)
+        {
+            Nom = nom;
+        }
+
+        public abstract void Impacter(Jardin jardin);
+    }
+}
